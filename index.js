08a424dee6f765ca7fa78bc5097cf90a9a306637
tragicDilemma <lspss9950101@gmail.com>
2019-01-20 12:43:37 +0800

    add_feature_image

--- index.js
+++ index.js
@@ -128,7 +128,9 @@ bot.on('message', function(event) {
 		}
 	}else if(event.message.type == 'image'){
 		if(ops.indexOf(event.source.userId) != -1){
-			var bytes = bot.getMessageContent(event.message.id);
+			var bytes = event.message.content().then(function (content) {
+				console.log(content.toString('base64'));
+			});
 			console.log(bytes);
 			var msg = {
 				type: 'image',
