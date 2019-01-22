c6bb9817593b11bbc5ebcbef4a792710ef4585bf
tragicDilemma <lspss9950101@gmail.com>
2019-01-23 03:34:59 +0800

    nation

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ function generate_list(team) {
 					},
 					{
 						"type": "text",
-						"text": occupied.toString,
+						"text": "occupied.toString",
 						"color": "#444444",
 						"size": "md",
 						"align": "end"
