b7745f037b6825f39324513ffa81befde3721b98
tragicDilemma <lspss9950101@gmail.com>
2019-01-23 03:37:17 +0800

    nation

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ function generate_list_ops() {
 			}
 		}
 	};
-	for (var i = 0; i < 11; i++) {
+	/*for (var i = 0; i < 11; i++) {
 		var status;
 		var list = "0一二三四五六七八"
 		if (occupation[i] == 0) status = "目前尚未被佔領";
@@ -238,7 +238,7 @@ function generate_list_ops() {
 			}]
 		}
 		msg.contents.body.contents = append + msg.contents.body.contents;
-	}
+	}*/
 	return msg;
 }
 
