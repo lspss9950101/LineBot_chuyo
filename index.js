d8f1a801d635b0473536e03ba27f4ec67663be3f
tragicDilemma <lspss9950101@gmail.com>
2019-01-23 12:40:17 +0800

    nation

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function generate_list(team) {
 	var minute = Math.floor(time / 1000 / 60) % 60;
 	var second = Math.floor(time / 1000) % 60;
 	var occupied = 0;
-	console.log(scroe[team]);
+	console.log(score[team]);
 	for (var i = 0; i < 11; i++)if (occupation[i] == team + 1) occupied++;
 	return {
 		"type": "flex",
